3cc577b119fa927d4a57c24078cee44e6c88b541
jacob <jacob.180524@nv.ccsd.net>
2019-10-11 13:44:06 -0700

    2

--- src/org/test/TestPlayer.java
+++ src/org/test/TestPlayer.java
@@ -10,7 +10,7 @@ public class TestPlayer extends GameObject {
 		animations = new Animation[3];
 		animations[0] = new Animation();
 		animations[0].frames = new ImageResource[3];
-		animations[0].frames[1] = new ImageResource("/res/Saved Pictures/tile000.png");
+		animations[0].frames[0] = new ImageResource("/res/Saved Pictures/tile000.png");
 		animations[1].frames[1] = new ImageResource("/res/Saved Pictures/tile001.png");
 		animations[2].frames[2] = new ImageResource("/res/Saved Pictures/tile002.png");
 	}
